6e84c81cb274725f21236ce9e894c6dc38c26b98
Kaitlin Naughten <kaight@bas.ac.uk>
2023-07-12 15:12:40 +0100

    Wrote coordinates_from_global

---
+++ make_domain.py
@@ -0,0 +1,38 @@
+import xarray as xr
+import numpy as np
+
+# Given a pre-existing global domain (eg eORCA025), slice out a regional domain.
+# Inputs:
+# global_file = path to a NetCDF file for the global domain. The domain_cfg file is ideal, but any file that includes all of these variables will work: nav_lon, nav_lat, e2f, e2v, e2u, e2t, e1f, e1v, e1u, e1t, gphif, gphiv, gphiu, gphit, glamf, glamv, glamu, glamt
+# out_file: path to desired output NetCDF file
+# imin, imax, jmin, jmax: optional bounds on the i and j indicies to slice. Uses the python convention of 0-indexing, and selecting the indices up to but not including the last value. So, jmin=0, jmax=10 will select the southernmost 10 rows.
+# nbdry: optional latitude of the desired northern boundary. The code will generate the value of jmax that corresponds to this on the zonal mean.
+def coordinates_from_global (global_file='/gws/nopw/j04/terrafirma/kaight/input_data/grids/domcfg_eORCA025_v3.nc', out_file='coordinates.nc', imin=None, imax=None, jmin=None, jmax=None, nbdry=None):
+
+    ds = xr.open_dataset(global_file)
+
+    if nbdry is not None:
+        # Choose the value of jmax based on the supplied latitude
+        if jmax is not None:
+            raise Exception('nbdry and jmax are both defined')
+        # Average over longitude to get a 1D field of "typical" latitude
+        lat_1d = ds['nav_lat'].mean(dim='x')
+        # Find the first index north of the given boundary
+        jmax = np.where(lat_1d > nbdry)[0][0]
+
+    # Now set default values for i and j slicing
+    if imin is None:
+        imin = 0
+    if imax is None:
+        imax = ds.sizes['x']
+    if jmin is None:
+        jmin = 0
+    if jmax is None:
+        jmax = ds.sizes['y']
+
+    # Now slice to these bounds
+    ds_regional = ds.isel(x=slice(imin,imax), y=slice(jmin, jmax))
+
+    # Select only the variables we need and write to file
+    var_names = ['nav_lon', 'nav_lat', 'e2f', 'e2v', 'e2u', 'e2t', 'e1f', 'e1v', 'e1u', 'e1t', 'gphif', 'gphiv', 'gphiu', 'gphit', 'glamf', 'glamv', 'glamu', 'glamt']
+    ds_regional[var_names].to_netcdf(out_file)
