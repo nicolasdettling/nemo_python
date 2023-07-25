6f4700e90ff93e3ad4d8604b29c00dec366ba5c4
Kaitlin Naughten <kaight@bas.ac.uk>
2023-07-25 11:34:20 +0100

    Interpolation seems to work with 2D loop but very slow

--- interpolation.py
+++ interpolation.py
@@ -5,9 +5,10 @@ from .utils import polar_stereo, fix_lon_range, extend_grid_edges
 def interp_cell_binning (source, nemo, plot=False, pster=True, periodic=True):
 
     from shapely.geometry import Point, Polygon
-    import time
+    import geopandas as gpd
+    from tqdm import trange
     if plot:
-        import matplotlib.pylot as plt
+        import matplotlib.pyplot as plt
 
     if pster:
         # Antarctic polar stereographic
@@ -28,13 +29,28 @@ def interp_cell_binning (source, nemo, plot=False, pster=True, periodic=True):
     # Pad x_f, y_f with an extra row and column on the west and south so we have all the grid cell edges. Note this changes the indexing convention relative to the t-grid.
     x_f = extend_grid_edges(x_f, 'f', periodic=periodic)
     y_f = extend_grid_edges(y_f, 'f', periodic=periodic)
+    
+    # Make a copy of the source dataset trimmed to the dimensions of nemo, to fill in later.
+    interp = source.isel(x=slice(0,nx), y=slice(0,ny))
+    # Make sure x and y are dummy axes, with 2D versions ready to fill in with real values
+    if len(interp['x'].shape) == 1:
+        y2d, x2d = xr.broadcast(interp['y'], interp['x'])
+        interp['x2d'] = x2d
+        interp['y2d'] = y2d
+    else:
+        interp['x2d'] = interp['x']
+        interp['y2d'] = interp['y']
+    interp = interp.assign_coords(x=np.arange(nx), y=np.arange(ny))
+    # Also add extra array showing the number of data points in each grid cell
+    interp = interp.assign({'num_points':interp['y2d']*0})
 
     print('Interpolating')
     # Loop over grid cells in NEMO...sorry.
     # Any vectorised way of doing this (using existing packages like cf or xesmf) overflows when you throw all of BedMachine3 at it. This could be solved using parallelisation, but a slower approach using less memory is preferable to asking users to sort out a SLURM job especially for something that won't be done very often (domain generation). Also NEMO is not CF compliant so even the existing tools are a headache when they require cell boundaries...
-    for j in range(ny):
-        print('...latitude index '+str(j+1)+' of '+str(ny))
-        for i in range(nx):
+    for j in trange(ny):
+        # First pass at narrowing down the source points to search: throw away anything that's too far south or north of the extent of this latitude row (based on grid corners). This will drastically speed up the search later.
+        source_search1 = source.where((source['y'] >= np.amin(y_f[j,:]))*(source['y'] <= np.amax(y_f[j+1,:])), drop=True)        
+        for i in trange(nx, leave=False):
             # Get the cell boundaries
             x_corners = np.array([x_f[j,i], x_f[j,i+1], x_f[j+1,i+1], x_f[j+1,i]])
             y_corners = np.array([y_f[j,i], y_f[j,i+1], y_f[j+1,i+1], y_f[j+1,i]])
@@ -45,19 +61,25 @@ def interp_cell_binning (source, nemo, plot=False, pster=True, periodic=True):
                 elif x_t[j,i] > 170 and np.amin(x_corners) < -170:
                     x_corners[x_corners < 0] += 360
             grid_cell = Polygon([(x_corners[n], y_corners[n]) for n in range(4)])
-            # Narrow down the source points to search. Since quadrilaterals are convex, we don't need to consider any points which are west of the westernmost grid cell corner, and so on.
-            source_search = source.where((source['x'] >= np.amin(x_corners))*(source['x'] <= np.amax(x_corners))*(source['y'] >= np.amin(y_corners))*(source['y'] <= np.amax(y_corners)), drop=True)
+            # Narrow down the source points to search again. Since quadrilaterals are convex, we don't need to consider any points which are west of the westernmost grid cell corner, and so on.
+            source_search2 = source_search1.where((source_search1['x'] >= np.amin(x_corners))*(source_search1['x'] <= np.amax(x_corners))*(source_search1['y'] >= np.amin(y_corners))*(source_search1['y'] <= np.amax(y_corners)), drop=True)
             # Use GeoPandas to find which points are in cell, then convert the list of points back to an xarray Dataset
             gdf_cell = gpd.GeoDataFrame(index=[0], geometry=[grid_cell], crs=crs) 
-            df = source_search.to_dataframe().reset_index()
+            df = source_search2.to_dataframe().reset_index()
             gdf_points = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['x'], df['y']), crs=crs)
             points_within = gpd.tools.sjoin(gdf_points, gdf_cell, predicate='within')
-            source_within = xr.Dataset.from_dataframe(points_within).drop(['index','index_right','geometry'])
+            if points_within.size == 0:
+                # There are no points within this grid cell
+                continue            
+            source_within = xr.Dataset.from_dataframe(points_within).drop(['index_right','geometry','time_counter'])
             source_mean = source_within.mean()
-
-            # Fill in dataset with same dimension as NEMO but same variables as source
-            # Deal with case where there are no points
-            # Plot diagnostics for number of points, error in mean x and y compared to t-point, mean of each variable
+            source_mean = source_mean.rename({'x':'x2d', 'y':'y2d'})
+            source_mean = source_mean.assign({'num_points':xr.DataArray(source_within.sizes['index'])})
+            # Now fill in this point in the interpolated dataset
+            interp = xr.where((interp.coords['x']==i)*(interp.coords['y']==j), source_mean, interp)
+    # Mask all variables where there are no points
+    
+    # Plot diagnostics for number of points, error in mean x and y compared to t-point, mean of each variable
             
             
         
