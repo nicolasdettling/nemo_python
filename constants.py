54611d1a4bfa953b29b20e1e945782d9690ecb66
Birgit <birgal@bas.ac.uk>
2023-10-25 14:47:41 +0100

    Update constants.py to add Bear Ridge bounds

--- constants.py
+++ constants.py
@@ -6,3 +6,15 @@ deg2rad = np.pi/180.0
 deg_string = r'$^{\circ}$'
 # 10^-3 formatted nicely in a string (for absolute salinity)
 gkg_string = r'10$^{-3}$'
+
+# Dictionary of bounds on different regions - some in 2 parts
+# lon_min, lon_max, lat_min, lat_max
+region_bounds = {
+    'bear_ridge_S': [-110.35, -110.35, -74.35, -73.8791],
+    'bear_ridge_N': [-112.05, -109.05, -73.8791, -72.7314],
+    }
+
+# Isobaths restricting some regions
+region_bathy_bounds = {
+    'bear_ridge_N': [350, None],
+}
