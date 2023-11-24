4dcc03719f469a5c2de1d24c1af8569df326227a
Kaitlin Naughten <kaight@bas.ac.uk>
2023-11-24 10:48:37 +0000

    Load dask arrays so shelf_mask isn't a disaster

--- timeseries.py
+++ timeseries.py
@@ -170,6 +170,7 @@ def update_simulation_timeseries (suite_id, timeseries_types, timeseries_file='t
     for file_pattern in nemo_files:
         print('Processing '+file_pattern)
         ds_nemo = xr.open_mfdataset(sim_dir+'/'+file_pattern)
+        ds_nemo.load()
         precompute_timeseries(ds_nemo, timeseries_types, sim_dir+'/'+timeseries_file, halo=halo)
                     
                 
