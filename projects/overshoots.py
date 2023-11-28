512b19abf017fd4c9088efa98e453854bae97faf
Kaitlin Naughten <kaight@bas.ac.uk>
2023-11-28 10:25:39 +0000

    Remove trailing slash if needed

--- projects/overshoots.py
+++ projects/overshoots.py
@@ -22,6 +22,9 @@ def update_overshoot_timeseries (suite_id, timeseries_file='timeseries.nc', base
 # Plot timeseries by region for all variables in the given suite ID, and show interactively.
 def plot_all_timeseries_by_region (suite_id, regions=['all', 'amundsen_sea', 'bellingshausen_sea', 'larsen', 'filchner_ronne', 'east_antarctica', 'amery', 'ross'], var_names=['massloss', 'bwtemp', 'bwsalt', 'cavity_temp', 'cavity_salt', 'shelf_temp', 'shelf_salt'], colours=None, timeseries_file='timeseries.nc', base_dir='./', fig_dir=None):
 
+    while suite_id.endswith('/'):
+        suite_id = suite_id[:-1]
+
     for var in var_names:
         timeseries_by_region(var, base_dir+'/'+suite_id+'/', colours=colours, timeseries_file=timeseries_file, fig_name=None if fig_dir is None else (fig_dir+'/'+var+'_'+suite_id+'.png'))
 
