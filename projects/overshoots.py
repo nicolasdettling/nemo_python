5849c59fc62076d23d15a9aee8b44528c74fbd3c
Kaitlin Naughten <kaight@bas.ac.uk>
2023-11-28 10:24:11 +0000

    Save timeseries to file

--- projects/overshoots.py
+++ projects/overshoots.py
@@ -20,9 +20,9 @@ def update_overshoot_timeseries (suite_id, timeseries_file='timeseries.nc', base
 
 
 # Plot timeseries by region for all variables in the given suite ID, and show interactively.
-def plot_all_timeseries_by_region (suite_id, regions=['all', 'amundsen_sea', 'bellingshausen_sea', 'larsen', 'filchner_ronne', 'east_antarctica', 'amery', 'ross'], var_names=['massloss', 'bwtemp', 'bwsalt', 'cavity_temp', 'cavity_salt', 'shelf_temp', 'shelf_salt'], colours=None, timeseries_file='timeseries.nc', base_dir='./'):
+def plot_all_timeseries_by_region (suite_id, regions=['all', 'amundsen_sea', 'bellingshausen_sea', 'larsen', 'filchner_ronne', 'east_antarctica', 'amery', 'ross'], var_names=['massloss', 'bwtemp', 'bwsalt', 'cavity_temp', 'cavity_salt', 'shelf_temp', 'shelf_salt'], colours=None, timeseries_file='timeseries.nc', base_dir='./', fig_dir=None):
 
     for var in var_names:
-        timeseries_by_region(var, base_dir+'/'+suite_id+'/', colours=colours, timeseries_file=timeseries_file)
+        timeseries_by_region(var, base_dir+'/'+suite_id+'/', colours=colours, timeseries_file=timeseries_file, fig_name=None if fig_dir is None else (fig_dir+'/'+var+'_'+suite_id+'.png'))
 
     
