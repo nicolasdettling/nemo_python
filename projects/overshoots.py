a809eb4db6505c5d9b97f69812240a727e9d0daa
Kaitlin Naughten <kaight@bas.ac.uk>
2023-12-12 14:05:34 +0000

    Loop through all simulations to update timeseries

--- projects/overshoots.py
+++ projects/overshoots.py
@@ -32,6 +32,14 @@ def update_overshoot_timeseries (suite_id, base_dir='./', domain_cfg='/gws/nopw/
     update_simulation_timeseries_um(suite_id, ['global_mean_sat'], timeseries_file='timeseries_um.nc', sim_dir=base_dir+'/'+suite_id+'/', stream='p5')
 
 
+# Call for all simulations (add to the list of suite IDs as needed)
+def update_overshoot_timeseries_all (suite_id, base_dir='./', domain_cfg='/gws/nopw/j04/terrafirma/kaight/input_data/grids/domcfg_eORCA1v2.2x.nc'):
+
+    # To add when ERROR_SINGLE_COPY_UNAVAILABLE is resolved (and these suites are re-pulled from MASS): cs495, cz855
+    for suite_id in ['cs568', 'cx209', 'cw988', 'cw989', 'cw990', 'cz826', 'cy837', 'cz834', 'da087', 'cy838', 'cz855', 'cz374', 'cz859', 'cz375', 'cz376', 'cz377', 'cz378', 'da697', 'cz944', 'da800']:
+        update_overshoot_timeseries(suite_id, base_dir=base_dir, domain_cfg=domain_cfg)
+
+
 # Plot timeseries by region for all variables in the given suite ID, and show interactively.
 def plot_all_timeseries_by_region (suite_id, regions=['all', 'amundsen_sea', 'bellingshausen_sea', 'larsen', 'filchner_ronne', 'east_antarctica', 'amery', 'ross'], var_names=['massloss', 'bwtemp', 'bwsalt', 'cavity_temp', 'cavity_salt', 'shelf_temp', 'shelf_salt'], colours=None, timeseries_file='timeseries.nc', base_dir='./', smooth=0, fig_dir=None):
 
