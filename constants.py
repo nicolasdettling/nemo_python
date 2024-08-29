1cacbacba94691131b5b0394a4190b2e90f08b81
Birgit Rogalla <birgal@bas.ac.uk>
2024-08-29 09:04:23 +0100

    CESM2-LE2 ensemble member names that have UBOT and VBOT as well

--- constants.py
+++ constants.py
@@ -147,6 +147,5 @@ line_colours = ['black', 'Crimson', 'blue', 'DarkMagenta', 'DimGrey', 'DarkGreen
 land_colour     = '#9999a3'
 iceshelf_colour = '#d4d5da' 
 
-# Keep track of ensemble members downloaded for all variables:
-cesm2_ensemble_members = ['1011.001','1021.002','1031.002','1041.003','1051.003','1061.004', \
-                          '1071.004','1081.005','1091.005'] 
+# Keep track of ensemble members downloaded for all variables: 
+cesm2_ensemble_members = ['1011.001','1031.002','1051.003','1071.004','1091.005'] 
