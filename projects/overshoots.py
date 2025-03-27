5e9ae816363bbae424b1f9068c50ef057bcbc0f7
Kaitlin Naughten <kaight@bas.ac.uk>
2025-03-27 10:48:37 +0000

    Working on static ice version

--- projects/overshoots.py
+++ projects/overshoots.py
@@ -1304,7 +1304,7 @@ def plot_bwtemp_massloss_by_gw_panels (base_dir='./', static_ice=False):
     if static_ice:
         fig_name += '_static_ice'
     fig_name += '.png'
-    finished_plot(fig) #, fig_name='figures/temp_massloss_by_gw_panels.png', dpi=300)
+    finished_plot(fig) #, fig_name=fig_name, dpi=300)
 
 
 # Calculate UKESM's bias in bottom salinity on the continental shelf of Ross and FRIS. To do this, find the global warming level averaged over 1995-2014 of a historical simulation with static cavities (cy691) and identify the corresponding 10-year period in each ramp-up ensemble member. Then, average bottom salinity over those years and ensemble members, compare to observational climatologies interpolated to NEMO grid, and calculate the area-averaged bias.
